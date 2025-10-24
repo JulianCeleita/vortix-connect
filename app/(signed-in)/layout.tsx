5c08159c1865965866e444cc6fc0328a7d1dc69d
Julian Celeita <andresceleita@yahoo.es>
2025-10-24 18:48:17 -0500

    Add signed-in dashboard and layout, update middleware
    Introduces a new dashboard page and layout for signed-in users. Updates middleware to protect dashboard routes using Clerk authentication.

---
+++ app/(signed-in)/layout.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+function SignedInLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
+
+export default SignedInLayout;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isAutheticatedRoute = createRouteMatcher(['/dashboard(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isAutheticatedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
